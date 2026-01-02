d2d501a0629e00f21d0e87eb3851a4c1073e1bd5
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-02 10:03:49 +0100

    ajustes

--- layouts/header.php
+++ layouts/header.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="en"><!-- Basic -->
+
 <head>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
-   
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
-     <!-- Site Metas -->
-    <title>Nazca - Restaurante y Pisco Bar</title>  
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+
+    <!-- Site Metas -->
+    <title>Nazca - Restaurante y Pisco Bar</title>
+    <meta name="keywords" content="Restaurante peruano, comida peruana, pisco bar, gastronomia peruana, ceviche, lomo saltado, anticuchos, comida tradicional peruana">
+    <meta name="description" content="Un lugar que te transporta a nuestro Perú tanto en su carta como en sus sabores. Por eso te ofrecemos comida tradicional.La cocina peruana es una combinación de influencias culturales gastronómicas que acaba siendo un deleite para los seguidores de la gastronomía peruana.">
+    <meta name="author" content="GeorkingWeb">
+    <link rel="shortcut icon" href="https://georkingweb.com/nazca/images/logo2.png" type="image/x-icon">
     <!-- Site Icons -->
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="images/favicon.ico">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">    
-	<!-- Site CSS -->
-    <link rel="stylesheet" href="css/style.css">    
-    <link rel="stylesheet" href="css/style2.css">    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="css/responsive.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/custom.css">
 
-    
+
 </head>
 
 <body class="body">
---
+++ prueba.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 'DIA SOLO MEDIA JORNADA: '.((8.5*5*3) + (8.5*6*3) + (9*8.5) )* 4;
+echo '<br>';
+echo 'DIA JORNADA COMPLETA: '.((8.5*8.5*3) + (8.5*9.5*3) + (9*8.5) )* 4;
+echo '<br>';
+echo 'NOCHE JORNADA COMPLETA: '.((8.5*3.5*5) + (8.5*9.5)  + (9*3.5) )* 4;
