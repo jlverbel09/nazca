6ce2aae4b71d54aecef0b1deb4ba2623e954d711
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-16 22:16:25 +0100

    ajustes

--- frase.php
+++ frase.php
@@ -4,10 +4,13 @@
 		<img src="./images/icono2.png" class="icono2" alt="">
 		<div class="row">
 			<div class="col-md-8 ml-auto mr-auto text-left">
-				<p class="lead ">
+				<!-- <p class="lead ">
 					" Más que un restaurante, una embajada de sabores. En NAZCA, honramos nuestras raíces para ofrecerte lo mejor de nuestra cultura. "
+				</p> -->
+				<p class="lead ">
+					Donde se mezclan a la perfección el placer y los sabores exóticos del Perú.
 				</p>
-				<span class="lead">Anderson</span>
+				<!-- <span class="lead">Anderson</span> -->
 			</div>
 		</div>
 	</div>
--- historia.php
+++ historia.php
@@ -9,13 +9,15 @@
 			<div class="col-lg-6 col-md-6 col-sm-12 text-center mt-5">
 				<div class="inner-column">
 					<h1 class="text-white">Bienvenido a <span class="text-principal">NAZCA</span></h1>
-					<h4 class="text-light">Pequeña Historia</h4>
+					<!-- <h4 class="text-light">Pequeña Historia</h4> -->
 					<p>Un lugar que te transporta a
 						nuestro Perú tanto en su carta como en sus sabores.
 						Por eso te ofrecemos comida tradicional. </p>
 					<p>La cocina peruana es una combinación de influencias
 						culturales gastronómicas que acaba siendo un deleite
 						para los seguidores de la gastronomía peruana.</p>
+
+					
 					<a class="btn btn-lg btn-circle btn-outline-new-white text-light" href="reservation.php">Hacer Reserva</a>
 				</div>
 			</div>
