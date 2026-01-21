adaa3a857c350472bacd21224565ceb22bd6f3fc
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-21 22:51:48 +0100

    ajustes

--- layouts/main.php
+++ layouts/main.php
@@ -19,7 +19,7 @@ $enlace = $url[2];
 						<li class="nav-item <?php if($enlace == 'menu.php'){ echo 'active';} ?>"><a class="nav-link" href="menu.php">Menú</a></li>
 						<li class="nav-item <?php if($enlace == 'about.php'){ echo 'active';} ?>"><a class="nav-link" href="about.php">Sobre Nosotros</a></li>
 						<li class="nav-item <?php if($enlace == 'reservation.php'){ echo 'active';} ?>"><a class="nav-link" href="reservation.php">Reservar</a></li>
-						<li class="nav-item <?php if($enlace == 'stuff.php'){ echo 'active';} ?>"><a class="nav-link" href="stuff.php">Nuestro Equipo</a></li>
+						<!-- <li class="nav-item <?php if($enlace == 'stuff.php'){ echo 'active';} ?>"><a class="nav-link" href="stuff.php">Nuestro Equipo</a></li> -->
 						<li class="nav-item <?php if($enlace == 'gallery.php'){ echo 'active';} ?>"><a class="nav-link" href="gallery.php">Galería</a></li>
 						<li class="nav-item <?php if($enlace == 'contact.php'){ echo 'active';} ?>"><a class="nav-link" href="contact.php">Contáctanos</a></li>
 						<!-- <li class="nav-item dropdown">
