71b3fd0d7225aa49e8e194853eec8a55bee64ceb
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-23 14:11:01 +0100

    ajustes

--- layouts/main.php
+++ layouts/main.php
@@ -7,7 +7,7 @@ $enlace = $url[2];
 	<header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
-				<a class="navbar-brand" href="index.php">
+				<a class="navbar-brand" href="index">
 					<img src="images/logo2.png" alt="" />
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,26 +15,26 @@ $enlace = $url[2];
 				</button>
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item <?php if($enlace == 'index.php'){ echo 'active';} ?>"><a class="nav-link" href="index.php">Inicio</a></li>
-						<li class="nav-item <?php if($enlace == 'menu.php'){ echo 'active';} ?>"><a class="nav-link" href="menu.php">Menú</a></li>
-						<li class="nav-item <?php if($enlace == 'about.php'){ echo 'active';} ?>"><a class="nav-link" href="about.php">Sobre Nosotros</a></li>
-						<li class="nav-item <?php if($enlace == 'reservation.php'){ echo 'active';} ?>"><a class="nav-link" href="reservation.php">Reservar</a></li>
-						<!-- <li class="nav-item <?php if($enlace == 'stuff.php'){ echo 'active';} ?>"><a class="nav-link" href="stuff.php">Nuestro Equipo</a></li> -->
-						<li class="nav-item <?php if($enlace == 'gallery.php'){ echo 'active';} ?>"><a class="nav-link" href="gallery.php">Galería</a></li>
-						<li class="nav-item <?php if($enlace == 'contact.php'){ echo 'active';} ?>"><a class="nav-link" href="contact.php">Contáctanos</a></li>
+						<li class="nav-item <?php if($enlace == 'index'){ echo 'active';} ?>"><a class="nav-link" href="index">Inicio</a></li>
+						<li class="nav-item <?php if($enlace == 'menu'){ echo 'active';} ?>"><a class="nav-link" href="menu">Menú</a></li>
+						<li class="nav-item <?php if($enlace == 'about'){ echo 'active';} ?>"><a class="nav-link" href="about">Sobre Nosotros</a></li>
+						<li class="nav-item <?php if($enlace == 'reservation'){ echo 'active';} ?>"><a class="nav-link" href="reservation">Reservar</a></li>
+						<!-- <li class="nav-item <?php if($enlace == 'stuff'){ echo 'active';} ?>"><a class="nav-link" href="stuff">Nuestro Equipo</a></li> -->
+						<li class="nav-item <?php if($enlace == 'gallery'){ echo 'active';} ?>"><a class="nav-link" href="gallery">Galería</a></li>
+						<li class="nav-item <?php if($enlace == 'contact'){ echo 'active';} ?>"><a class="nav-link" href="contact">Contáctanos</a></li>
 						<!-- <li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="reservation.php">Reservation</a>
-								<a class="dropdown-item" href="stuff.php">Stuff</a>
-								<a class="dropdown-item" href="gallery.php">Gallery</a>
+								<a class="dropdown-item" href="reservation">Reservation</a>
+								<a class="dropdown-item" href="stuff">Stuff</a>
+								<a class="dropdown-item" href="gallery">Gallery</a>
 							</div>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="blog.php">blog</a>
-								<a class="dropdown-item" href="blog-details.php">blog Single</a>
+								<a class="dropdown-item" href="blog">blog</a>
+								<a class="dropdown-item" href="blog-details">blog Single</a>
 							</div>
 						</li> -->
 					</ul>
